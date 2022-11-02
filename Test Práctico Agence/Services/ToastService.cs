145a79c1e635694ca1bbc723cdd0b3c27566e2b8
EduardoSalazar252 <74371423+EduardoSalazar252@users.noreply.github.com>
2022-11-02 15:45:25 -0400

    5th upload

---
+++ Test Práctico Agence/Services/ToastService.cs
@@ -0,0 +1,24 @@
+﻿namespace Test_Práctico_Agence.Services
+{
+    public enum ToastLevel
+    {
+        Info,
+        Success,
+        Warning,
+        Error
+    }
+    public class ToastService
+    {
+        public event Action<string, ToastLevel> OnShow;
+        public event Action OnHide;
+
+        public void ShowToast(string message, ToastLevel level)
+        {
+            OnShow?.Invoke(message, level);
+        }
+        public void HideToast()
+        {
+            OnHide?.Invoke();
+        }
+    }
+}
